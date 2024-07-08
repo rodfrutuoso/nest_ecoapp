95f5a669ae44d20824c2b7e81f7ae330d4ea6d2c
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-07-08 16:14:33 -0300

    auth controller finished

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,31 +1,45 @@
-import { ConflictException, UsePipes } from "@nestjs/common";
+import {
+  ConflictException,
+  UnauthorizedException,
+  UsePipes,
+} from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { BigQueryService } from "src/bigquery/bigquery.service";
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 import { z } from "zod";
 import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
 import { JwtService } from "@nestjs/jwt";
 
-// const createAccountBodySchema = z
-//   .object({
-//     name: z.string(),
-//     email: z.string().email(),
-//     password: z.string(),
-//   })
-//   .required();
+const authenticateBodySchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+  .required();
 
-// type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
 @Controller("/sessions")
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(private jwt: JwtService, private bigquery: BigQueryService) {}
 
   @Post()
-  // @HttpCode(201)
-  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  // async handle(@Body() body: CreateAccountBodySchema) {
-  async handle() {
-    const token = this.jwt.sign({ sub: "user-id" });
-    return token;
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
+
+    const [user] = await this.bigquery.user.select({ where: { email } });
+
+    if (!user) throw new UnauthorizedException("Dados de login incorretos");
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid)
+      throw new UnauthorizedException("Dados de login incorretos");
+
+    const token = this.jwt.sign({ sub: user.id });
+
+    return { access_token: token };
   }
 }
