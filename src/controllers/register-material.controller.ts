6f6a4978bd6f5de2c4b181f70c096da1aed16df4
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-07-10 16:06:33 -0300

    user payload decorator

--- src/controllers/register-material.controller.ts
+++ src/controllers/register-material.controller.ts
@@ -1,10 +1,13 @@
-import { ConflictException, UseGuards, UsePipes } from "@nestjs/common";
+import { ConflictException, Req, UseGuards, UsePipes } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { BigQueryService } from "src/bigquery/bigquery.service";
 import { hash } from "bcryptjs";
 import { z } from "zod";
 import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Request } from "express";
+import { CurrentUser } from "src/auth/current-user.decorator";
+import { UserPayload } from "src/auth/jwt-strategy.guard";
 
 // const registerMaterialBodySchema = z
 //   .object({
@@ -25,7 +28,8 @@ export class RegisterMaterialController {
   //   @HttpCode(201)
   //   @UsePipes(new ZodValidationPipe(registerMaterialBodySchema))
   //   async handle(@Body() body: RegisterMaterialBodySchema) {
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
     return "ok";
   }
 }
