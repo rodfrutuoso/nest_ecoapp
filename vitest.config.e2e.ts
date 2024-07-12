e3264c92d0eb8843b19dd593aade64fc3661ab27
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-07-12 09:45:05 -0300

    vitest working

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,16 +1,16 @@
 import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
+import tsConfigParh from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
-    include:['**/*.e2e-spec.ts'],
+    include: ["**/*.e2e-spec.ts"],
     globals: true,
     root: "./",
   },
   plugins: [
-    // This is required to build the test files with SWC
+    tsConfigParh(),
     swc.vite({
-      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
       module: { type: "es6" },
     }),
   ],
