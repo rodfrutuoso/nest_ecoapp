d016d827c1e57964a61e24567f4a244bf15b8013
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-07-31 13:19:36 -0300

    fineshed bigquery repositories

--- src/infra/database/bigquery/repositories/bq-physical-document-repository.ts
+++ src/infra/database/bigquery/repositories/bq-physical-document-repository.ts
@@ -3,6 +3,7 @@ import { PaginationParams } from "src/core/repositories/pagination-params";
 import { PhysicalDocumentRepository } from "src/domain/material-movimentation/application/repositories/physical-document-repository";
 import { PhysicalDocument } from "src/domain/material-movimentation/enterprise/entities/physical-document";
 import { BigQueryService } from "../bigquery.service";
+import { BqPhysicalDocumentMapper } from "../mappers/bq-physical-document-mapper";
 
 @Injectable()
 export class BqPhysicalDocumentRepository
@@ -11,30 +12,56 @@ export class BqPhysicalDocumentRepository
   constructor(private bigquery: BigQueryService) {}
 
   async create(physicalDocument: PhysicalDocument): Promise<void> {
-    throw new Error("Method not implemented.");
+    const data = BqPhysicalDocumentMapper.toBigquery(physicalDocument);
+
+    await this.bigquery.physicalDocument.create([data]);
   }
 
   async findByIdentifier(identifier: number): Promise<PhysicalDocument | null> {
-    throw new Error("Method not implemented.");
+    const [physicalDocument] = await this.bigquery.physicalDocument.select({
+      where: { identifier },
+    });
+
+    if (!physicalDocument) return null;
+
+    return BqPhysicalDocumentMapper.toDomin(physicalDocument);
   }
 
   async findByID(id: string): Promise<PhysicalDocument | null> {
-    throw new Error("Method not implemented.");
+    const [physicalDocument] = await this.bigquery.physicalDocument.select({
+      where: { id },
+    });
+
+    if (!physicalDocument) return null;
+
+    return BqPhysicalDocumentMapper.toDomin(physicalDocument);
   }
 
   async save(physicalDocument: PhysicalDocument): Promise<void> {
-    throw new Error("Method not implemented.");
+    await this.bigquery.physicalDocument.update({
+      data: BqPhysicalDocumentMapper.toBigquery(physicalDocument),
+      where: { id: BqPhysicalDocumentMapper.toBigquery(physicalDocument).id },
+    });
   }
-  
+
   async findMany(
-    params: PaginationParams,
+    { page }: PaginationParams,
     identifier?: number,
     projectId?: string
   ): Promise<PhysicalDocument[]> {
-    throw new Error("Method not implemented.");
+    const pageCount = 40;
+
+    const physicalDocuments = await this.bigquery.physicalDocument.select({
+      where: { projectId, identifier },
+      limit: pageCount,
+      offset: pageCount * (page - 1),
+      orderBy: { column: "identifier", direction: "ASC" },
+    });
+
+    return physicalDocuments.map(BqPhysicalDocumentMapper.toDomin);
   }
-  
+
   async delete(physicalDocumentId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    await this.bigquery.physicalDocument.delete({ id: physicalDocumentId });
   }
 }
--- src/infra/database/bigquery/repositories/bq-project-repository.ts
+++ src/infra/database/bigquery/repositories/bq-project-repository.ts
@@ -1,16 +1,36 @@
 import { Injectable } from "@nestjs/common";
 import { ProjectRepository } from "src/domain/material-movimentation/application/repositories/project-repository";
 import { Project } from "src/domain/material-movimentation/enterprise/entities/project";
+import { BigQueryService } from "../bigquery.service";
+import { BqProjectMapper } from "../mappers/bq-project-mapper";
 
 @Injectable()
 export class BqProjectRepository implements ProjectRepository {
-  findByProjectNumber(project_number: string): Promise<Project | null> {
-    throw new Error("Method not implemented.");
+  constructor(private bigquery: BigQueryService) {}
+
+  async findByProjectNumber(project_number: string): Promise<Project | null> {
+    const [project] = await this.bigquery.project.select({
+      where: { project_number },
+    });
+
+    if (!project) return null;
+
+    return BqProjectMapper.toDomin(project);
   }
-  findByID(id: string): Promise<Project | null> {
-    throw new Error("Method not implemented.");
+
+  async findByID(id: string): Promise<Project | null> {
+    const [project] = await this.bigquery.project.select({
+      where: { id },
+    });
+
+    if (!project) return null;
+
+    return BqProjectMapper.toDomin(project);
   }
-  create(project: Project): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async create(project: Project): Promise<void> {
+    const data = BqProjectMapper.toBigquery(project);
+
+    await this.bigquery.project.create([data]);
   }
 }
