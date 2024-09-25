5d63685e94de3bb1b1cbd5c1313b4e152dccbf71
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2024-09-25 17:07:06 -0300

    deleted unused storekeeper and estimator repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { BigQueryModule } from "./bigquery/bigquery.module";
-import { BqStorekeeperRepository } from "./bigquery/repositories/bq-storekeeper-repository";
 import { BqBaseRepository } from "./bigquery/repositories/bq-base-repository";
 import { BqBudgetRepository } from "./bigquery/repositories/bq-budget-repository";
 import { BqContractRepository } from "./bigquery/repositories/bq-contract-repository";
@@ -9,15 +8,12 @@ import { BqMovimentationRepository } from "./bigquery/repositories/bq-movimentat
 import { BqPhysicalDocumentRepository } from "./bigquery/repositories/bq-physical-document-repository";
 import { BqProjectRepository } from "./bigquery/repositories/bq-project-repository";
 import { MaterialRepository } from "src/domain/material-movimentation/application/repositories/material-repository";
-import { StorekeeperRepository } from "src/domain/material-movimentation/application/repositories/storekeeper-repository";
 import { BaseRepository } from "src/domain/material-movimentation/application/repositories/base-repository";
 import { BudgetRepository } from "src/domain/material-movimentation/application/repositories/budget-repository";
 import { ContractRepository } from "src/domain/material-movimentation/application/repositories/contract-repository";
 import { MovimentationRepository } from "src/domain/material-movimentation/application/repositories/movimentation-repository";
 import { PhysicalDocumentRepository } from "src/domain/material-movimentation/application/repositories/physical-document-repository";
 import { ProjectRepository } from "src/domain/material-movimentation/application/repositories/project-repository";
-import { EstimatorRepository } from "src/domain/material-movimentation/application/repositories/estimator-repository";
-import { BqEstimatorRepository } from "./bigquery/repositories/bq-estimator-repository";
 import { UserRepository } from "src/domain/material-movimentation/application/repositories/user-repository";
 import { BqUserRepository } from "./bigquery/repositories/bq-user-repository";
 
@@ -26,8 +22,6 @@ import { BqUserRepository } from "./bigquery/repositories/bq-user-repository";
   exports: [
     BigQueryModule,
     { provide: UserRepository, useClass: BqUserRepository },
-    { provide: StorekeeperRepository, useClass: BqStorekeeperRepository },
-    { provide: EstimatorRepository, useClass: BqEstimatorRepository },
     { provide: MaterialRepository, useClass: BqMaterialRepository },
     { provide: BaseRepository, useClass: BqBaseRepository },
     { provide: BudgetRepository, useClass: BqBudgetRepository },
@@ -41,8 +35,6 @@ import { BqUserRepository } from "./bigquery/repositories/bq-user-repository";
   ],
   providers: [
     { provide: UserRepository, useClass: BqUserRepository },
-    { provide: StorekeeperRepository, useClass: BqStorekeeperRepository },
-    { provide: EstimatorRepository, useClass: BqEstimatorRepository },
     { provide: MaterialRepository, useClass: BqMaterialRepository },
     { provide: BaseRepository, useClass: BqBaseRepository },
     { provide: BudgetRepository, useClass: BqBudgetRepository },
